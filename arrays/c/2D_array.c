cb2202a74425e63204a2f1584c728080eb75f99a
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-12-18 22:54:12 -0300

    two-dimensional array in C

--- arrays/c/2D_array.c
+++ arrays/c/2D_array.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void static_2D_array()
 {
@@ -9,8 +10,8 @@ void static_2D_array()
                 {7, 8, 9},
         };
 
-        size_t line_size = sizeof(sarr) / sizeof(sarr[0]);
-        size_t colm_size = sizeof(sarr[0]) / sizeof(int);
+        uint line_size = sizeof(sarr) / sizeof(sarr[0]);
+        uint colm_size = sizeof(sarr[0]) / sizeof(int);
 
         for (int line = 0; line < line_size; line++) {
                 for (int colm = 0; colm < colm_size; colm++) {
@@ -22,8 +23,8 @@ void static_2D_array()
 
 void dynamic_2D_array()
 {
-        size_t line_size = 3;
-        size_t colm_size = 4;
+        uint line_size = 3;
+        uint colm_size = 4;
 
         int **darr = malloc(line_size * sizeof(int **));
 
@@ -55,10 +56,57 @@ void dynamic_2D_array()
         }
 }
 
+static void *get_index(void **arr, size_t size, uint line, uint column)
+{
+        // return ((char *) arr[line]) + size * column;
+        return *(((char **) arr) + line) + column * size;
+}
+
+void generic_dynamic_2D_array()
+{
+        uint line_size = 3;
+        uint colm_size = 4;
+
+        size_t type_size = sizeof(int);
+
+        void **darr = malloc(line_size * sizeof(void **));
+
+        for (int i = 0; i < line_size; i++) {
+                darr[i] = malloc(colm_size * type_size);
+        }
+
+        for (int line = 0; line < line_size; line++) {
+                for (int colm = 0; colm < colm_size; colm++) {
+                        void *p = get_index(darr, type_size, line, colm);
+                        int value = line + colm;
+                        memmove(p, &value, type_size);
+                }
+        }
+
+        for (int line = 0; line < line_size; line++) {
+                for (int colm = 0; colm < colm_size; colm++) {
+                        printf("%d\t", *(int *) get_index(darr, type_size, line, colm));
+                }
+                printf("\n");
+        }
+
+        if (darr != NULL) {
+                for (int i = 0; i < line_size; i++) {
+                        if (darr[i] != NULL) {
+                                free(darr[i]);
+                                darr[i] = NULL;
+                        }
+                }
+                free(darr);
+                darr = NULL;
+        }
+}
+
 int main(int argc, char *argv[])
 {
         static_2D_array();
         dynamic_2D_array();
+        generic_dynamic_2D_array();
 
         return EXIT_SUCCESS;
 }
