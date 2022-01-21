047813af535947c5d1455ffdd891853848860bf5
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-21 01:45:56 -0300

    Fibonacci using Recursion in C

--- recursion/c/fibonacci.c
+++ recursion/c/fibonacci.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
-int recursive_fib(int nnumber)
+long long recursive_fib(long long nnumber)
 {
         if (nnumber < 0)
                 return -1;
@@ -13,7 +14,7 @@ int recursive_fib(int nnumber)
         return recursive_fib(nnumber - 2) + recursive_fib(nnumber - 1);
 }
 
-int loop_fib(int nnumber)
+long long loop_fib(int nnumber)
 {
         if (nnumber < 0)
                 return -1;
@@ -21,20 +22,19 @@ int loop_fib(int nnumber)
         if (nnumber <= 1)
                 return nnumber;
 
-        int prev = 0, next = 1;
+        long long arr[nnumber];
+        arr[0] = 0;
+        arr[1] = 1;
 
-        for (int total = 2; total <= nnumber; total++) {
-                int aux = prev;
-                prev = next;
-                next = aux + prev;
-        }
+        for (long long i = 2; i <= nnumber; i++)
+                arr[i] = arr[i - 1] + arr[i - 2];
 
-        return next;
+        return arr[nnumber];
 }
 
-int cache[100];
+static long long cache[10000];
 
-int memo_fib(int nnumber)
+long long memo_fib(int nnumber)
 {
         if (nnumber < 0)
                 return -1;
@@ -57,14 +57,35 @@ int memo_fib(int nnumber)
 
 int main(int argc, char *argv[])
 {
-        const int position = 10;
+        const int position = 35;
 
-        printf("%d\n", recursive_fib(position)); /* 55 */
-        printf("%d\n", loop_fib(position));      /* 55 */
+        struct timeval  tv1, tv2;
+
+        gettimeofday(&tv1, NULL);
+        printf("%lld\n", recursive_fib(position));      /* 9227465 */
+        gettimeofday(&tv2, NULL);
+
+        printf("Total time = %f seconds\n",
+               (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+               (double) (tv2.tv_sec - tv1.tv_sec));
+
+        gettimeofday(&tv1, NULL);
+        printf("%lld\n", loop_fib(position));           /* 9227465 */
+        gettimeofday(&tv2, NULL);
+
+        printf("Total time = %f seconds\n",
+               (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+               (double) (tv2.tv_sec - tv1.tv_sec));
 
         memset(cache, -1, sizeof(cache));
 
-        printf("%d\n", memo_fib(position));      /* 55 */
+        gettimeofday(&tv1, NULL);
+        printf("%lld\n", memo_fib(position));           /* 9227465 */
+        gettimeofday(&tv2, NULL);
+
+        printf("Total time = %f seconds\n",
+               (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+               (double) (tv2.tv_sec - tv1.tv_sec));
 
         return EXIT_SUCCESS;
 }
